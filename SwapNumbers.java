943f3626f9980b87c3917917d38f171929b35fd9
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-03-07 23:31:45 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,22 @@
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 10, num2 = 20;
+
+        System.out.println("--Before swap--");
+        System.out.println("num1 number = " + num1);
+        System.out.println("num2 number = " + num2);
+
+        
+        int temp = num1;
+
+        num1 = num2;
+
+        num2 = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("num1 number = " + num1);
+        System.out.println("num2 number = " + num2);
+    }
+}
