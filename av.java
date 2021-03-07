8b8ebab380a7f08a80a3b8d1c81bccc24d2bcba4
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-03-07 23:36:43 +0530

    Add files via upload

---
+++ av.java
@@ -0,0 +1,9 @@
+public class av {
+
+    public static void main(String[] args) {
+
+        char ch = 'p';
+        int castAscii = (int) ch;
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
